d6ae0821efb4ef8b83dd9cf9a30b9197d9b0a01b
gideonK-dev <gideonlkarbolo@gmail.com>
2026-03-17 16:50:44 +0300

    update src

--- src/component/GetProducts.jsx
+++ src/component/GetProducts.jsx
@@ -1,9 +1,85 @@
 import React from 'react'
+import { useState,useEffect } from 'react'
+import axios from 'axios'
+
+
 
 const GetProducts = () => {
+
+// initialize our hooks
+const[loading,setLoading]=useState("")
+const[error,setError]=useState("")
+const[products,setProducts]=useState([])
+
+const image_url="https://gideonk.alwaysdata.net/static/images/"
+
+// function to fetch data from API
+const fetchProducts=async()=>{
+
+  setLoading("Please wait as we retrieve your products")
+  try {
+
+  const response=await axios.get("https://gideonk.alwaysdata.net/api/getproductdetails")
+
+  console.log("The response is",response);
+  
+  setProducts(response.data)
+  setLoading("")
+
+    
+  } catch (error) {
+    setLoading("")
+    setError(error.message)
+    
+  }
+
+  // call our API
+ 
+}
+
+// end of function where we call useEffect
+
+useEffect(()=>{
+  fetchProducts()
+},{})
+
   return (
-    <div>
-        <h1>Welcome to Getproducts component</h1>
+    <div className='row'>
+      <h1>Available Products</h1>
+
+      <p className='text-warning'>{loading}</p>
+      <p className='text-danger'>{error}</p>
+
+
+      {/* calling .map to iterate through each item */}
+
+      {products.map((product)=>(
+
+
+      
+
+
+      <div className='col-md-3 justify-content-center'>
+
+        <div className='card shadow m-3'>
+          <img src={image_url +product.product_photo} alt="cake" className='product_img mt-3'/>
+
+          <div className='card-body'>
+            <h5 className='text-success'>{product.product_name}</h5>
+            <p className='text-secondary'>{product.product_description}</p>
+            <p className='text-warning'>{product.product_cost}</p>
+            <input type="button" className='btn btn-secondary w-100' value="Make Payment"/>
+
+          </div>
+
+
+
+        </div>
+
+      </div>
+      ))}
+
+      
     </div>
   )
 }
